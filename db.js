8a470732aad33f2e7d6db6c43cad1a33739823e1
sachinsurve2699 <sachinsurve2699@gmail.com>
2024-02-04 19:15:42 +0530

    hos

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const mysql = require("mysql")
 const connection = mysql.createConnection({
   user: "root",
   password: "root",
-  host: "localhost",
+  host: "127.0.0.1",
   port: 3306,
   database: "ve3",
 })
