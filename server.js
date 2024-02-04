4289aa5d38c9fca40d576fb33a78a655e95aaa5e
sachinsurve2699 <sachinsurve2699@gmail.com>
2024-02-04 19:37:08 +0530

    por

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.use(cors("*"))
 app.use("/user", routerUser)
 app.use("/tasks", routerTasks)
 
-app.listen(5000, "127.0.0.1", () => {
-  console.log("Server started at port  5000")
+app.listen(6000, "127.0.0.1", () => {
+  console.log("Server started at port  6000")
 })
